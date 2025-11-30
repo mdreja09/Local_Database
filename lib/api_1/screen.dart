c5e803459dc950fb3a69b633766d94281d00407d
Reja <almialsama@gmail.com>
2025-11-30 15:20:23 +0600

    Motivation Quotes

--- lib/api_1/screen.dart
+++ lib/api_1/screen.dart
@@ -15,33 +15,71 @@ class _MotivationalQuotesState extends State<MotivationalQuotes> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.green,
-        title: Text("Motivation Quotes",
-          style: TextStyle(fontSize: 26,fontWeight: FontWeight.w800),),
+        title: Text(
+          "Motivation Quotes",
+          style: TextStyle(fontSize: 26, fontWeight: FontWeight.w800),
+        ),
       ),
       body: Column(
         children: [
-        TextField(
+          //   TextField(
+          //
+          //
+          //   decoration: InputDecoration(
+          //    // labelText: "Email",
+          //     prefixIcon: Icon(Icons.search),
+          //
+          //     fillColor: Colors.grey,
+          //
+          //     border: OutlineInputBorder(
+          //
+          //       borderRadius: BorderRadius.circular(10),
+          //     ),
+          //
+          //
+          //   ),
+          // ),
+          Expanded(
+            child: ListView.builder(
 
-
-        decoration: InputDecoration(
-         // labelText: "Email",
-          prefixIcon: Icon(Icons.search),
-
-          fillColor: Colors.grey,
-
-          border: OutlineInputBorder(
-
-            borderRadius: BorderRadius.circular(10),
-          ),
-
-          //filled: true,
-        ),
-      ),
-          ListView.builder(itemBuilder: )
-
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 15,vertical: 20),
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Color(0xffF7F7F7),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 40),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Image.asset("assets/colon_1.png"),
+                          title: Text(
+                            "Honesty is the best policy",
+                            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                          ),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Unknown",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
         ],
-      ) ,
-
+      ),
     );
   }
 }
