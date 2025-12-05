1c05753dd4f69d0fe1516f2e2f8f461e2b2c1bf6
mdreja09 <muhammadreja0099@gmail.com>
2025-12-05 22:47:55 +0600

    try_catch

--- lib/api_1/api_json.dart
+++ lib/api_1/api_json.dart
@@ -3,12 +3,22 @@ import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 
-class Api {
-  getDataFromApi () async{
-    var url = Uri.parse("https://appapi.coderangon.com/api/slider");
-    var response = await http.get(url);
-    log("===${jsonDecode(response.body)}");
-    return jsonDecode(response.body);
+ class Api {
+  Future <Map> getDataFromApi () async{
+    try {
+      var url = Uri.parse("https://appapi.coderangon.com/api/slider");
+      var response = await http.get(url);
+      log("===${jsonDecode(response.body)}");
+      return jsonDecode(response.body);
+
+
+
+    }catch(error){
+      log("=====error : $error");
+    }
+    return {};
+
+
   }
 
 }
